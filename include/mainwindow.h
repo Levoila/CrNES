2bc0f9db16a1de9ad44c8bfb81e31ba2a6e8b09c
Felix Prevost <fepre@polymtl.ca>
2016-07-18 20:19:00 -0700

    Project base

---
+++ include/mainwindow.h
@@ -0,0 +1,25 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QtWidgets>
+
+#include <array>
+#include <cstdint>
+
+class MainWindow : public QOpenGLWidget
+{
+public:
+	MainWindow();
+	~MainWindow();
+	
+private:
+	void paintGL() override;
+	void resizeGL(int w, int h) override;
+	void initializeGL() override;
+	
+	static constexpr int nesWidth = 256;
+	static constexpr int nesHeight = 240;
+	std::array<uint8_t, nesWidth * nesHeight * 3> pixels;
+};
+
+#endif
---
+++ src/mainwindow.cpp
@@ -0,0 +1,45 @@
+#include "mainwindow.h"
+
+#include <GL/gl.h>
+#include <QDebug>
+
+#include <iostream>
+
+MainWindow::MainWindow()
+{
+	pixels.fill(128);
+}
+
+MainWindow::~MainWindow()
+{
+	
+}
+
+void MainWindow::initializeGL()
+{
+	QOpenGLFunctions* f = QOpenGLContext::currentContext()->functions();
+	f->glClearColor(1.0f, 1.0f, 0.0f, 1.0f);
+	
+	glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+void MainWindow::resizeGL(int w, int h)
+{
+    qDebug() << "Resize to " << "(" << w << ", " << h << ")";
+	QOpenGLFunctions* f = QOpenGLContext::currentContext()->functions();
+	
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+    glOrtho(0.0f, w, 0.0f, h, -1.0, 1.0);
+}
+
+void MainWindow::paintGL()
+{
+	QOpenGLFunctions* f = QOpenGLContext::currentContext()->functions();
+	f->glClear(GL_COLOR_BUFFER_BIT);
+	
+	glDrawPixels(nesWidth, nesHeight, GL_RGB, GL_UNSIGNED_BYTE, pixels.data());
+
+    qDebug() << "paint";
+}
